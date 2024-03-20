1f7f8a57676b4f8547a5fadafe537e6510fb935a
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-03-20 15:42:56 +0100

    #3: change default font to Poppins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        { rel: "preload", href: "https://fonts.googleapis.com" },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap",
+        },
+      ],
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    },
+  },
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
