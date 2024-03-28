4526a10f940e586bcbd9ad549c869067b6617ada
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-03-28 15:50:46 +0100

    #6: create basic implementation of word search generator

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="antialiased bg-gray-50">
+    <div class="antialiased">
       <nav
         class="bg-white border-b border-gray-200 px-4 py-2.5 fixed left-0 right-0 top-0 z-50"
       >
@@ -48,7 +48,7 @@
         </div>
       </aside>
 
-      <main class="p-4 md:ml-80 h-auto pt-20 ">
+      <main class="p-4 md:ml-80 h-auto pt-20">
         <slot />
       </main>
     </div>
