6fc61e8dc9a265271dd81d5da3dbde03a3515039
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-03-29 19:38:59 +0100

    #14: change navbar button style

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -16,7 +16,7 @@
         >
         <NuxtLink
           to="/signup"
-          class="text-black bg-gray-100 border-2 border-black focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          class="text-white bg-black focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center"
           >Get started</NuxtLink
         >
       </div>
